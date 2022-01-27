ceee1eca46d835815e5a68c362e2739afa61527d
fjebaker <fergusbkr@gmail.com>
2022-01-27 14:18:56 +0000

    table for metoffice spot forecast

--- database-setup/initfile.sql
+++ database-setup/initfile.sql
@@ -22,6 +22,33 @@ CREATE TABLE energy_onsite(
     PRIMARY KEY ( entry_id )
 );
 
+CREATE TABLE metoffice_spot(
+    entry_id INT NOT NULL AUTO_INCREMENT,
+
+    time DATETIME,
+    
+    windDirectionFrom10m INT,
+    visibility INT,
+    mslp INT,
+    uvIndex INT,
+    significantWeatherCode INT,
+    precipitationRate INT,
+    totalPrecipAmount INT,
+    totalSnowAmount INT,
+    probOfPrecipitation INT,
+    screenRelativeHumidity DECIMAL(8,2),
+    windGustSpeed10m DECIMAL(8,2),
+    max10mWindGust DECIMAL(8,2),
+    windSpeed10m DECIMAL(8,2),
+    screenTemperature DECIMAL(4,2),
+    maxScreenAirTemp DECIMAL(4,2),
+    minScreenAirTemp DECIMAL(4,2),
+    screenDewPointTemperature DECIMAL(4,2),
+    feelsLikeTemperature DECIMAL(4,2),
+
+    PRIMARY KEY ( entry_id )
+);
+
 GRANT ALL PRIVILEGES ON llanwrytd.* TO 'aimlac'@'%';
 
 FLUSH PRIVILEGES;
